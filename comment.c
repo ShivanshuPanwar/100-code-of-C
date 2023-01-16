96e8eccaabc747bfe027c035615776d696b23b62
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-01-17 00:18:44 +0530

    Add files via upload

---
+++ comment.c
@@ -0,0 +1,10 @@
+/* Write comments for programs a & b. */
+#include<stdio.h>
+int main()
+{
+    //  int side;
+    // printf("enter side:",side);
+    // scanf("%d",&side);
+    // printf("area of square:%d",side*side); 
+    return 0;
+}
