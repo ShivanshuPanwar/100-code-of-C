e38770ba98f9258fe6c71eec48f6f36631906371
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-01-16 23:46:39 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,20 @@
+//variables
+#include<stdio.h>
+int main() 
+{
+/*int number;
+int age;
+int price; */
+
+int age = 22;
+float pi = 3.14;
+char percentage = '%';
+
+printf("age is %d\n", age);
+printf("pi is %f\n", pi);
+printf("percentage is %c", percentage);
+
+
+return 0;
+
+}
