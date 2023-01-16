a2ebf386d87472a5bb1acb157d1bd3d9276e7003
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-01-16 23:55:49 +0530

    Add files via upload

---
+++ arofci.c
@@ -0,0 +1,10 @@
+//area of circle=pi*r*r
+#include<stdio.h>
+int main()
+{
+    int radius;
+    printf("enter radius of circle:",radius);
+    scanf("%d",&radius);
+    printf("area of circle:%f",3.14*radius*radius);
+    return 0;
+}
