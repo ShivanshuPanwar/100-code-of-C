5d5585ce84427eaed7141a91acbcb99d540daff1
Shivanshu Panwar <118122773+ShivanshuPanwar@users.noreply.github.com>
2023-01-16 23:57:08 +0530

    Add files via upload

---
+++ arofsq.c
@@ -0,0 +1,15 @@
+//area of square
+#include<stdio.h>
+int main()
+{
+    int side;
+    printf("enter side:",side);
+    scanf("%d",&side);
+    printf("area of square:%d",side*side);
+    
+   /* This is also a method to find square
+    scanf("%d",&side);
+    printf("%d",side*side); */
+    
+    return 0;
+}
